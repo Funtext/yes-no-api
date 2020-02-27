937e7185059c79855430c522c5aaf77bee83170a
Ma110509 <m.ceccon05@gmail.com>
2020-02-27 18:52:06 +0100

    It did not

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import random
-import requests
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS, cross_origin
 
@@ -12,7 +11,4 @@ data = ["yes", "no"]
 @app.route('/')
 @cross_origin()
 def one_post():
-    url = 'https://api.giphy.com/v1/gifs/random'
-    myobj = {'api_key': 'iMDy2jSTrxPi3WcTa1pwcHbf4BYq9HWH'}
-    x = requests.post(url, data = myobj)
-    return random.choice(data) + x
+    return random.choice(data)
