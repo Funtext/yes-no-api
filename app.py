af67314448abddecade607aabd065c530ce27ae3
Ma110509 <m.ceccon05@gmail.com>
2020-02-27 18:46:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,4 @@ data = ["yes", "no"]
 @app.route('/')
 @cross_origin()
 def one_post():
-    r = requests.get(url="https://api.giphy.com/v1/gifs/random?api_key=iMDy2jSTrxPi3WcTa1pwcHbf4BYq9HWH")
-    data = r.json()
-    gifLink = data['data']['image_url']
-    return "{ \"response\":\"" + random.choice(data) + "\", \"gifLink\":\"" + gifLink + "\"}"
+    return random.choice(data)
